a7c2680b47a8f06c415058467afc99cb66f24aab
Retre4t <antonandersen98@yahoo.dk>
2023-09-25 08:29:56 +0200

    Quick fix
    hurtige fixes - applikation starter ikke op i Expo.

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
-// index.js
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import NavigationBar from './NavigationBar'; // Import your Navigation component
-//import Navigation from './NavigationBar'; // Import your Navigation component
+import Navigation from './Navigation'; // Import your Navigation component
 
 const App = () => {
-  return <NavigationBar />;
+  return <Navigation />;
 };
 
-AppRegistry.registerComponent('HundeTinder', () => App); // Replace 'YourAppName' with your app's name
-
-
+AppRegistry.registerComponent('HundeTinder', () => App); // Replace 'HundeTinder' with your app's name
